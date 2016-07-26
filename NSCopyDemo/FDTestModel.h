28b17acf7027672c5a975e2c224f61815078183f
Master-fd <asus@asusdeMac-Pro.local>
2016-07-26 21:44:32 +0800

    OK

---
+++ NSCopyDemo/FDTestModel.h
@@ -0,0 +1,13 @@
+//
+//  FDTestModel.h
+//  NSCopyDemo
+//
+//  Created by asus on 16/7/26.
+//  Copyright (c) 2016年 asus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FDTestModel : NSObject
+
+@end
---
+++ NSCopyDemo/FDTestModel.m
@@ -0,0 +1,13 @@
+//
+//  FDTestModel.m
+//  NSCopyDemo
+//
+//  Created by asus on 16/7/26.
+//  Copyright (c) 2016年 asus. All rights reserved.
+//
+
+#import "FDTestModel.h"
+
+@implementation FDTestModel
+
+@end
